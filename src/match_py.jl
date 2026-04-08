5c35487b6cfaa66b814462c8696c3b088d67bdd2
jverzani <jverzani@gmail.com>
2026-04-08 11:53:04 -0400

    wip

--- src/match_py.jl
+++ src/match_py.jl
@@ -212,6 +212,7 @@ end
 
 # match non_variable_patterns
 # return iterator of (ss, ps, σ) or nothing
+
 function _match_non_variable_patterns(ss, ps, fₐ=nothing, σ=match_dict())
     #@show :mnvp, ss, ps, fₐ, σ
 
@@ -225,10 +226,15 @@ function _match_non_variable_patterns(ss, ps, fₐ=nothing, σ=match_dict())
     n == 0 && return ((ss, ps, σ), )
     n ≤ length(ss) || return nothing
 
-    # XXX could tighten this up, does some excess checking
+    # XXX Tighten this up, does some excess checking
     # look at example on p20
     # (pat, sub) = (:(g(a, ~x) + g(~x, ~y) + g(~(~z))), :(g(a, b) + g(b, a) + g(a, c)))
-    # should write using prewalk traversal
+    # with `permutations`, we consider all of paths 123,132,213,231,312,321 in sequence,
+    # so we end up checking
+    # 123,13⋅,2⋅⋅, 2⋅⋅, 31⋅, 32⋅ -- that is 11 checks
+    # where as we only need to check
+    # 1[23,3⋯], 2[⋅⋅,⋅⋅], 3[1⋅,2⋅] which is only 8 checks were we to walk in pre-order fashion
+
     i = permutations(1:length(ss), n)
 
     λ = inds -> begin
